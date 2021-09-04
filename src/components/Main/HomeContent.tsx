45469b792a10d831c96356b8f05cf673008f9a3f
feliper <feliper@bellunofidc.com.br>
2021-09-04 15:18:36 -0300

    Adds footer social media links

--- src/components/Main/HomeContent.tsx
+++ src/components/Main/HomeContent.tsx
@@ -106,9 +106,36 @@ const InaVideoContainer = styled.div`
 export const Footer = styled.footer`
   background: #fba147 0% 0% no-repeat padding-box;
   opacity: 1;
+  overflow: hidden;
+
+  .footer-social-container{
+    display: inline-block;
+    margin-left: 120px;
+    margin-top, margin-bottom: 20px;
+  }
+
+  p{
+      text-align: left;
+      font: normal normal normal 35px/43px Montserrat;
+      letter-spacing: 0px;
+      color: #4F415C;
+  }
+
+  .fa {
+    margin-right: auto;
+    margin-top: auto;
+  }
+
+  .footer-img-container{
+    display: inline-block;
+    float: right;
+    margin-right: 80px;
+    margin-top, margin-bottom: 20px;
+  }
 
   .footer-img {
-    margin: auto;
+    margin: 20px;
+    width: 300px;
   }
 `;
 
@@ -183,11 +210,25 @@ const HomeContent = (): JSX.Element => {
         </InaVideoContainer>
       </LoreContainer>
       <Footer>
-        <img
-          alt="mini-ina"
-          className="footer-img"
-          src={`${process.env.PUBLIC_URL}/MiniIna.png`}
-        />
+        <div className="footer-social-container">
+          <a href="https://twitter.com/ninomaeinanis">
+            <p>
+              <i className="fa fa-twitter" style={{ fontSize: "48px", color: "#4F415C" }}></i> @ninomaeinanis
+            </p>
+          </a>
+          <a rel="stylesheet" href="https://www.youtube.com/channel/UCMwGHR0BTZuLsmjY_NT5Pwg">
+            <p>
+              <i className="fa fa-youtube-play" style={{ fontSize: "48px", color: "#4F415C" }}></i> Ninomae Ina&apos;nis Ch.
+            </p>
+          </a>
+        </div>
+        <div className="footer-img-container">
+          <img
+            alt="mini-ina"
+            className="footer-img"
+            src={`${process.env.PUBLIC_URL}/MiniIna.png`}
+          />
+        </div>
       </Footer>
     </div>
   );
