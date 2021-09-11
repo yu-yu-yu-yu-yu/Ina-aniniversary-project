244a9471a5e2eb20081602247d6705fd6bef0c31
yuyu <yuyuyuyuyuyuyuyuyuyu@protonmail.com>
2021-09-11 20:57:14 +0300

    Credits draft

--- src/components/Main/HomeFooter.tsx
+++ src/components/Main/HomeFooter.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import ReactDOM from "react-dom";
+import { Backdrop } from "../Timeline/styles/List";
 
 const Footer = styled.footer`
   position: relative;  
@@ -101,13 +103,12 @@ const AoLogo = styled.div`
     margin-top: -4.6em;
   }
 
-  img{
+  img {
     width: 120px;
 
     @media only screen and (max-width: 1200px) {
       width: 80px;
     }
-  
   }
 
   @media only screen and (max-width: 950px) {
@@ -115,9 +116,97 @@ const AoLogo = styled.div`
   }
 `;
 
+const CreditsModal = styled.div`
+  width: 600px;
+  height: 343px;
+  margin: 70px auto 0;
+  right: 0;
+  left: 0;
+  top: 0;
+  position: fixed;
+  background-color: var(--inai-purple);
+  border-radius: 15px;
+  z-index: 69;
+`;
+
+const Credits = ({
+  visible,
+  hideModal,
+}: {
+  visible?: boolean;
+  hideModal: () => void;
+}) => {
+  if (!visible) return null;
+  return ReactDOM.createPortal(
+    <>
+      <Backdrop onClick={hideModal} />
+      <CreditsModal>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={3}>Credits</th>
+            </tr>
+          </thead>
+          <tr>
+            <td>yuyu</td>
+            <td>ﾕﾕ#2434</td>
+            <td>Developer</td>
+          </tr>
+          <tr>
+            <td>@Kimchi</td>
+            <td>Kinji#8200</td>
+            <td>Developer</td>
+          </tr>
+          <tr>
+            <td>Frogo</td>
+            <td>losfroger#8927</td>
+            <td>Art assets,Design</td>
+          </tr>
+          <tr>
+            <td>@Braincell #0901 | swoog</td>
+            <td>swoog#0901</td>
+            <td>Art assets, General Contribution</td>
+          </tr>
+          <tr>
+            <td>Lynn</td>
+            <td>Lynn#0572</td>
+            <td>Planning, General Contribution</td>
+          </tr>
+          <tr>
+            <td>Braincell #88 | Unidachi88</td>
+            <td>Uni#9971</td>
+            <td>General Contribution</td>
+          </tr>
+          <tr>
+            <td>Braincell #3698 || Rift</td>
+            <td>Rift#6565</td>
+            <td>General Contribution</td>
+          </tr>
+
+          <tr>
+            <td>Ninomae Ina’nis</td>
+            <td>@ninomaeinanis</td>
+            <td>Ina L2D Homepage art</td>
+          </tr>
+          <tr>
+            <td>Shikabashi</td>
+            <td>@Shikabashi</td>
+            <td>Ina L2D Homepage animation</td>
+          </tr>
+          <tr>
+            <td>Graphic design</td>
+            <td />
+            <td>My passion</td>
+          </tr>
+        </table>
+      </CreditsModal>
+    </>,
+    document.getElementById("root") as HTMLElement
+  );
+};
 
 const HomeFooter = (): JSX.Element => {
-
+  const [creditsVisible, setCreditsVisible] = useState(false);
   return (
     <Footer>
       <AoLogo>
@@ -132,22 +221,32 @@ const HomeFooter = (): JSX.Element => {
             <i className="fa fa-twitter"></i> @ninomaeinanis
           </p>
         </a>
-        <a rel="stylesheet" href="https://www.youtube.com/channel/UCMwGHR0BTZuLsmjY_NT5Pwg">
+        <a
+          rel="stylesheet"
+          href="https://www.youtube.com/channel/UCMwGHR0BTZuLsmjY_NT5Pwg"
+        >
           <p>
             <i className="fa fa-youtube-play"></i> Ninomae Ina&apos;nis Ch.
           </p>
         </a>
       </div>
-      <div className="footer-img-container">
+      <div
+        className="footer-img-container"
+        onClick={() => setCreditsVisible(true)}
+      >
         <img
           alt="mini-ina"
           className="footer-img"
           src={`${process.env.PUBLIC_URL}/MiniIna.png`}
         />
       </div>
+
+      <Credits
+        visible={creditsVisible}
+        hideModal={() => setCreditsVisible(false)}
+      />
     </Footer>
   );
 };
-
 
 export default HomeFooter;
